3170df4fe20c1551a5ac3960663f83fa8c422aac
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-23 11:29:41 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,25 @@
+﻿// Чертоусова ПЕ
+// 22.10.2022
+// Задание 1 ДЗ
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count = 0;
+    int A;
+    cin >> A;
+    while (A > 0)
+    {
+        count++;
+        A /= 10;
+    }
+    cout << count << endl;
+    if (A == 0) {count = 1;
+}
+    
+    
+}
+
+
